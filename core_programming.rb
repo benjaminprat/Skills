ad54e10b8aa74f6a0915787e38525153b816caba
Ben Prat <bvprat@gmail.com>
2021-02-17 10:18:49 -0700

    Complete conditionals #4

--- core_programming.rb
+++ core_programming.rb
@@ -32,3 +32,13 @@ if num1 < 10 && num2 < 10
 else
   print 0
 end
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+number = 9001
+
+if number > 9000
+  print 1
+else
+  print -1
+end
