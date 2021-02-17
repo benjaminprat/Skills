721d79efb5a397241c35a5e0145f5bf76174f832
Ben Prat <bvprat@gmail.com>
2021-02-17 11:09:02 -0700

    Complete loops #2

--- core_programming.rb
+++ core_programming.rb
@@ -281,10 +281,18 @@ end
 
 #loops
 
-#Write a while loop to print the numbers 1 through 10.
+#1 Write a while loop to print the numbers 1 through 10.
 
 num = 1
 while num < 11
   puts num
   num += 1
+end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+i = 1
+while i < 6
+  puts "hello"
+  i += 1
 end
