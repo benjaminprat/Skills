f7adbe514948683be443e78c7a251e5e23c1a77e
Ben Prat <bvprat@gmail.com>
2021-02-17 10:20:32 -0700

    Complete conditionals #7

--- core_programming.rb
+++ core_programming.rb
@@ -67,3 +67,13 @@ if num1 > 10 || num2 > 10
 else
   print -100
 end
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+num = 2
+
+if num < 0
+  print 1776
+else
+  print 1979
+end
