2592e7730c04cd4cf47154316d77ab40b62dd35b
Ben Prat <bvprat@gmail.com>
2021-02-17 12:14:24 -0700

    Complete arrays #7

--- core_programming.rb
+++ core_programming.rb
@@ -421,3 +421,9 @@ while i < names.length
   puts names[i]
   i += 1
 end
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+seasons = ["spring", "summer"]
+seasons << "fall"
+print seasons
