0dbdfed40489cd17570698396fc27d9cd99bede7
Ben Prat <bvprat@gmail.com>
2021-02-17 10:28:47 -0700

    Complete concat/interpolation #4

--- core_programming.rb
+++ core_programming.rb
@@ -141,3 +141,11 @@ if input == "marco"
 else
   print "nope"
 end
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+color1 = "green"
+color2 = "yellow"
+color3 = "purple"
+
+print "three of my favorite colors are" + " " + color1 + "," + " " + color2 + "," + " " + "and" + " " + color3 + "."
