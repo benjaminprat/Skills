5c85170560d53d5f8022749ec22721eb6f0b58d5
Ben Prat <bvprat@gmail.com>
2021-02-17 10:16:27 -0700

    Initial commit.Complete conditionals #1

---
+++ core_programming.rb
@@ -0,0 +1,11 @@
+# if, elsif, else
+
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+number = 2
+
+if number == 10
+  print 0
+else
+  print -1
+end
