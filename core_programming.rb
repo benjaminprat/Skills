30214e78c21601a6a3433c5b537d2bd4a83a60cb
Ben Prat <bvprat@gmail.com>
2021-02-17 12:18:12 -0700

    Complete arrays #9

--- core_programming.rb
+++ core_programming.rb
@@ -433,3 +433,12 @@ print seasons
 numbers = [1, 2, 3, 4, 5]
 numbers[0] = numbers[0] * 10
 print numbers
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [39, 30]
+i = 0
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end
