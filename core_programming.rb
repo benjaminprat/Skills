6317aed4f00284165ec21ceab074bcf74d2fc1f3
Ben Prat <bvprat@gmail.com>
2021-03-02 12:22:21 -0700

    "Complete excercise #8"

--- core_programming.rb
+++ core_programming.rb
@@ -534,6 +534,14 @@ p laptop[:year]
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
+laptops = [
+  {brand: "Apple", model:"MacBook Pro"},
+  {brand: "Dell", model: "X600"},
+  {brand: "Hewlett Packard", model: "X200"}
+]
+
+p laptops[1][:model]
+
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
