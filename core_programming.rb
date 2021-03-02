42ad950088871844986d47072e92e9c4c6d769f6
Ben Prat <bvprat@gmail.com>
2021-03-02 12:14:59 -0700

    "Complete excercise #5"

--- core_programming.rb
+++ core_programming.rb
@@ -500,6 +500,16 @@ p book[:language]
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
+books = [
+  {title: "Naked", author: "David Sedaris"},
+  {title: "On The Road", author: "Jack Kerouac"},
+  {title: "The Giving Tree", author: "Shel Silverstein"}
+]
+
+p books[2][:author]
+
+
+
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
