f2ec1ac4be487baafdc9a1a6e1248c093324dc31
Ben Prat <bvprat@gmail.com>
2021-03-09 14:35:33 -0700

    "Add select exercise number 2"

--- core_programming.rb
+++ core_programming.rb
@@ -741,6 +741,11 @@ p numbers.select {|number| n < 20 }
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
+words = ["winner", "winner", "winner", "chicken", "dinner"]
+
+p words.select {|word| word[0] == "w"}
+
+
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
