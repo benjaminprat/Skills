c7bf81851c002d11b8c3296e7cc557f0cf36c0d5
Ben Prat <bvprat@gmail.com>
2021-03-09 14:42:26 -0700

    "Add select exercise number 5"

--- core_programming.rb
+++ core_programming.rb
@@ -759,10 +759,14 @@ numbers = [2, 4, 5, 1, 8, 9, 7]
 
 p numbers.select {|number| number.even?}
 
-
-
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+
+p strings.select {|string| string.length < 4}
+
+
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
