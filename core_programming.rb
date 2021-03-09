76a71d3ebac91e7d7dbf617ad85cbdeb8d8ea17b
Ben Prat <bvprat@gmail.com>
2021-03-09 14:51:16 -0700

    "Add select exercise number 10"

--- core_programming.rb
+++ core_programming.rb
@@ -797,3 +797,7 @@ p items.select {|item| item[:price] < 10}
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+
+p numbers.select {|number| number.odd?}
