e6d9d99595ef69cfb4c1f4e0e98d876aca386867
Ben Prat <bvprat@gmail.com>
2021-03-09 14:49:50 -0700

    "Add select exercise number 9"

--- core_programming.rb
+++ core_programming.rb
@@ -791,5 +791,9 @@ p strings.select{|string| string[0] != "b"}
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+p items.select {|item| item[:price] < 10}
+
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
